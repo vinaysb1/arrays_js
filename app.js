601bf29d4663ae2d4b4eadd8cd0082c51654034d
vinay <vinaykumarbeelagi@gmail.com>
2022-11-24 12:30:29 +0530

    basics of array

---
+++ app.js
@@ -0,0 +1,16 @@
+cars = ["volvo","bmw","ford"];
+console.log(cars[0]);
+console.log(cars[1]);
+console.log(cars[2]);
+
+const num1 = [2,4,6,8,10];
+for(let i=0;i<num1.length;i++){
+console.log(num1[i]);
+}
+
+index = 0;
+num2 = [1,5,7,9,11,15];
+while (index<num2.length){
+    console.log(num2[index]);
+    index++;
+}
