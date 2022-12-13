1f127f10d00341a481c0f68f1853b07dd7871b5e
vinay <vinaykumarbeelagi@gmail.com>
2022-12-13 22:54:28 +0530

    print pivot index of an array

--- app.js
+++ app.js
@@ -102,8 +102,8 @@ const emp4 = [
     { id: 3, name: "usha", age: 4, year: 2018 },
     { id: 4, name: "vidya", age: 5, year: 2017 },
 ];
-var i=0;
-while(i<emp4.length){
+var i = 0;
+while (i < emp4.length) {
     console.log(emp4[i].name);
     i++;
 }
@@ -114,22 +114,22 @@ const emp5 = [
     { id: 3, name: "Naveen", age: 28, dep: "cs" },
     { id: 4, name: "guru", age: 22, dep: "cv" }
 ];
-var i=0;
-do{
+var i = 0;
+do {
     console.log(emp5[i].age)
     i++;
 }
-while(i<emp5.length);
+while (i < emp5.length);
 //map() method
 var myArray = [
-    {id: 1, firstName: "venu", lastName: 'gopal', age: 12},
-    {id: 2, firstName: 'Jane', lastName: 'Brown', age: 14},
-    {id: 3, firstName: 'Martin', lastName: 'loother', age: 13},
-    {id: 4, firstName: 'subhas', lastName: 'bose', age: 17},
-    {id: 5, firstName: 'syam', lastName: 'sundar', age: 11}
+    { id: 1, firstName: "venu", lastName: 'gopal', age: 12 },
+    { id: 2, firstName: 'Jane', lastName: 'Brown', age: 14 },
+    { id: 3, firstName: 'Martin', lastName: 'loother', age: 13 },
+    { id: 4, firstName: 'subhas', lastName: 'bose', age: 17 },
+    { id: 5, firstName: 'syam', lastName: 'sundar', age: 11 }
 ];
 
-var newArray = myArray.map(function(profile, index, myArr) {
+var newArray = myArray.map(function (profile, index, myArr) {
     var newProfile = {
         'id': index + 1,
         'fullName': profile.firstName + ' ' + profile.lastName,
@@ -139,35 +139,35 @@ var newArray = myArray.map(function(profile, index, myArr) {
     return newProfile
 })
 
-newArray.forEach(function(profile, index, myArr) {
+newArray.forEach(function (profile, index, myArr) {
     console.log(profile.fullName)
 });
 
-for (let i=0;i<10;i++){
+for (let i = 0; i < 10; i++) {
     console.log("Hello world");
 }
 
-for (let i=1;i<11;i++){
+for (let i = 1; i < 11; i++) {
     console.log(i);
 }
 // print even no's range 0-20 using for loop
-for(let i=0;i<=20;i+=2){
+for (let i = 0; i <= 20; i += 2) {
     console.log(i);
 }
 let sum = 0;
-for(let i=1;i<11;i++){   
-    sum = sum+i;
+for (let i = 1; i < 11; i++) {
+    sum = sum + i;
     console.log(sum);
 }
 
-const array = [3,6,1,5,9,4,7,10,11];
+const array = [3, 6, 1, 5, 9, 4, 7, 10, 11];
 const even = [];
 const odd = [];
-for(let i=0;i<array.length;i++){
-    if (i%2==0){
-    even.push(array[i])
+for (let i = 0; i < array.length; i++) {
+    if (i % 2 == 0) {
+        even.push(array[i])
     }
-    else{
+    else {
         odd.push(array[i])
     }
 
@@ -190,15 +190,15 @@ for (let i = 0; i < num15.length; i++) {
         max = num15[i]
     }
 }
-console.log(max); 
+console.log(max);
 
 
 // print 0 and 1 with in the array
-let num16 = [0,1,0,1,1,1,0,0];
+let num16 = [0, 1, 0, 1, 1, 1, 0, 0];
 var zero = 0;
 var ones = 1;
-for(i=0;i<num16.length;i++){
-    if(num16[i]==0){
+for (i = 0; i < num16.length; i++) {
+    if (num16[i] == 0) {
         zero++;
     }
     else {
@@ -209,29 +209,46 @@ console.log(zero);
 console.log(ones);
 
 //count the total number of item repeated in a array
-const num17 = [0,1,1,1,0,1,2,3,1,2,1,0,1,0,3,3];
+const num17 = [0, 1, 1, 1, 0, 1, 2, 3, 1, 2, 1, 0, 1, 0, 3, 3];
 let count0 = 0;
 let count1 = 1;
 let count2 = 2;
 let count3 = 3;
-for(i=0;i<num17.length;i++){
-    if(num17[i]==0){
+for (i = 0; i < num17.length; i++) {
+    if (num17[i] == 0) {
         count0++;
     }
-    else if(num17[i]==1){
+    else if (num17[i] == 1) {
         count1++;
     }
-    else if(num17[i]==2){
+    else if (num17[i] == 2) {
         count2++;
+    }
+    else count3++;
 }
-      else count3++;
-}
-console.log(count0,count1,count2,count3);      
+console.log(count0, count1, count2, count3);
 
-//for in loop example
-const stu = {name:"vinay",age:35,class:8}
-for(let key in stu){
-    console.log(`${key}: => ${stu[key]}`);
+const worker = [
+    { name: "harish", age: 33, sal: "1000" },
+    { name:"vicky", age:23, sal:"2000"},
+    { name:"joe", age:34, sal:"1000"}
+];
+for (let profile of worker){
+    console.log(profile.sal);
 }
+// print pivot index of an array
+const arr2 = [6, 2,1, 2, 6];
+function pivotIndex(arr) {
+    leftSum = 0;
+    total = arr.reduce((a, b) => a + b);
+    rightSum = total
+    for (let i = 0; i < arr.length; i++) {
+        rightSum = rightSum - arr[i]
+        if (leftSum === rightSum) { return i }
+        leftSum = leftSum + arr[i];
+    }
+    return -1;
+}
+console.log(pivotIndex(arr2));
 
 
